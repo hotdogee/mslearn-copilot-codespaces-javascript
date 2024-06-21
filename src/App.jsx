f0aeb2dabcc0dc731fcf2d35b9cbc8ef112ca56d
Han Lin <hotdogee@gmail.com>
2024-06-21 10:41:59 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Han Lin",
+  title: "AI Research Engineer",
+  email: "hotdogee@gmail.com",
+  gitHub: "hotdogee",
+  instagram: "hotdogee",
+  linkedIn: "hotdogee",
+  medium: "hotdogee",
+  twitter: "hotdogee",
+  youTube: "hotdogee",
 };
 
 const primaryColor = "#4E567E";
